2188ef5d0eaa910189950cd3c88806570a74de7f
mleonelli <mleonelli@INFO-211-12>
2021-10-26 17:58:42 +0200

    avancement clock view

--- Clock2021Etu/src/main/java/clockIHM/ClockViewHMS.java
+++ Clock2021Etu/src/main/java/clockIHM/ClockViewHMS.java
@@ -1,15 +1,31 @@
 package clockIHM;
 
+
+
 import clockController.ClockController;
 import clockModel.ClockModel;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 
 public class ClockViewHMS extends ClockView {
 
 	public ClockViewHMS(String label, ClockModel tm, ClockController tc, int posX, int posY) {
 		super(label, tm, tc, posX, posY);
-				
+		HBox hbox = new HBox(10);
+		hbox.setStyle("-fx-background-color: BLUE;");
+		hbox.setAlignment(Pos.BOTTOM_CENTER);
+		
+		Button buttonPlus = new Button("+");
+		buttonPlus.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//myModel.addHourObserver();
+		});
 	}
 
 	@Override
